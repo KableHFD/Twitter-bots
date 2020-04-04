96de83e3d3376480951bb6734cebe7a24db53fe1
KableHFD <61998623+KableHFD@users.noreply.github.com>
2020-04-04 11:56:54 -0500

    Update search_bot.py

--- search_bot.py
+++ search_bot.py
@@ -1,8 +1,8 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('x8d96c5QiGfC5uWkuqXsogiam', 'ZxEStKAeb0XDUvLRIhba8yPX0zXMOdIQlykZGh0rcq8LQsb3oF')
-auth.set_access_token('1243219581143040001-Unj48Tn5tEvonBHPFOWOL5BXlgzlpM', 'WawL3Yy4cSkhgJsD1hy18TeIy2HO0dZ4kQfKCzZhyLSgm')
+auth = tweepy.OAuthHandler('*************************', '**************************************************')
+auth.set_access_token('**************************************************', '*********************************************')
 
 api = tweepy.API(auth)
 user = api.me()
